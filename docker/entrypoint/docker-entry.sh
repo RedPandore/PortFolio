6f6a439e1b3ae8f2bc8dd810c1223010750b3587
RedPandore <houry9@gmail.com>
2023-02-07 15:55:11 +0100

    asset install

--- docker/entrypoint/docker-entry.sh
+++ docker/entrypoint/docker-entry.sh
@@ -7,6 +7,7 @@ php bin/console doctrine:database:create --if-not-exists
 php bin/console doctrine:schema:update --force
 
 php bin/console ckeditor:install
+php bin/console assets:install
 
 php bin/console cache:clear
 php bin/console cache:warmup
